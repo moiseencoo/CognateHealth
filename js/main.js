6bfb9d8a024440aff31f12ceae6035efe14d48e8
moiseencoo <95varya@mail.ru>
2017-04-19 18:28:22 +0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+$('#main-slider').owlCarousel( {
+
+			  //Pagination
+	      pagination : true,
+	      paginationNumbers: false,
+	      mouseDrag : false,
+          touchDrag : true,
+	 
+	    // Responsive
+		  items : 1,
+		  singleItem: true,
+		  autoplay: true
+	}
+);
+
+
+$('#officers-slider').owlCarousel( {
+
+			  //Pagination
+	      pagination : true,
+	      paginationNumbers: false,
+	      mouseDrag : true,
+          touchDrag : true,
+	 
+	    // Responsive
+		  items : 2,
+		  autoplay: true, 
+		  navigation: true,
+		  navigationText: false
+	}
+);
