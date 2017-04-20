4e16d0ab53d4c1f458937aa21bc0cc8b8666cd34
moiseencoo <95varya@mail.ru>
2017-04-20 18:30:29 +0500

    Day2

--- js/main.js
+++ js/main.js
@@ -27,6 +27,22 @@ $('#officers-slider').owlCarousel( {
 		  items : 2,
 		  autoplay: true, 
 		  navigation: true,
-		  navigationText: false
+		  navigationText: false,
+		  itemsDesktop : [1199,2],
+  		  itemsDesktopSmall : [980,2],
+   		  itemsTablet: [768,1],
+   		  itemsMobile : [479,1]
 	}
 );
+
+
+$('#professionals-slider').owlCarousel({
+	items: 4, 
+	pagination: false,
+	navigation: true,
+	mouseDrag: true,
+	itemsDesktop : [1199,3],
+    itemsDesktopSmall : [980,2],
+    itemsTablet: [768,1],
+    itemsMobile : [479,1]
+});
